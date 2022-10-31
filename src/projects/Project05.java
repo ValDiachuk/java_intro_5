88c7a731da9cad3efb43230f4cc479d48681ebec
Valentyna Diachuk <valdiachuk9@gmail.com>
2022-10-31 12:18:37 -0500

    added fixed Project05

--- src/projects/Project05.java
+++ src/projects/Project05.java
@@ -33,17 +33,6 @@ public class Project05 {
 
     }
 
-    /*
-    Write a method that takes an int array that has at least one element. Find
-    the greatest and smallest elements from the array and print them.
-    Complete task using sort() method.
-    Test data:
-    [10, 7, 7, 10, -3, 10, -3]
-    Expected output:
-    Smallest = -3
-    Greatest = 10
-     */
-
     public static void findGreatestAndSmallestWithSort(int[] numbers) {
 
         Arrays.sort(numbers);
